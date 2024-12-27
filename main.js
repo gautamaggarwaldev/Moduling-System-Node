fdba1dc97d00a740e2cd1eae1f65cdcb492cf476
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-12-27 19:18:51 +0530

    es6 npm

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const searching = require('./searching.js');
+import { LinearSearch } from "./searching.js";
 
-console.log(searching);
-
-console.log(searching.LinearSearch([3,4,5,6,9,7,0],5));
-console.log(searching.BinarySearch([1,5,6,7,8,9,12],9));
+console.log(LinearSearch([3,4,5,6,9,7,0],5));
